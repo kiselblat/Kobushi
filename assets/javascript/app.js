4f961e6b5228c9bb1fbd109ccbd45dc4ac8aebf8
Tom Christ <44477304+kiselblat@users.noreply.github.com>
2019-05-25 13:56:32 -0500

    Set up document ready and add comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,33 @@
+var searchTerm = "";
+var searchLimit = 5;
+
 // function to search the jikan api for Naruto and log the object and title of the first hit
- var performSearch = function() {
-  var queryURL = "https://api.jikan.moe/v3/search/anime?q=Naruto&limit=5";
-   $.ajax({
-    url : queryURL,
-    method : "GET"
-   }).then(getResults);
- };
+var performSearch = function(term , limit) {
+var queryURL = "https://api.jikan.moe/v3/search/anime?q=" + term + "&limit=" + limit;
+  $.ajax({
+  url : queryURL,
+  method : "GET"
+  }).then(getResults);
+};
 
- var getResults = function(reply) {
-   var searchResults = reply.results;
-   console.log(searchResults);
-   for (var i = 0 ; i < searchResults.length ; i++) {
-     displayResults(searchResults[i]);
-   }
- }
+// Take in JSON response from performSearch() and parse out the searchResults
+var getResults = function(reply) {
+  var searchResults = reply.results;
+  console.log(searchResults);
+  for (var i = 0 ; i < searchResults.length ; i++) {
+    displayResults(searchResults[i]);
+  }
+};
 
- var displayResults = function(result) {
-   console.log("------------------------");
-   console.log(result.title);
-   console.log(result.image_url);
-   console.log(result.synopsis);
-   console.log("------------------------");
- }
+// displayResults() to the console (needs to be to the document)
+var displayResults = function(result) {
+  console.log("------------------------");
+  console.log(result.title);
+  console.log(result.image_url);
+  console.log(result.synopsis);
+};
 
- performSearch();
+$(document).ready(function() {
+  searchTerm = "Naruto"
+  performSearch(searchTerm , searchLimit);
+});
