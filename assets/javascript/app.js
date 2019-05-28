9876e5162447ae40af29a62c5ed36cd6a6db5e29
einstein1967 <bootcamp.ejb@gmail.com>
2019-05-28 16:54:32 -0500

    search working
    space between results; img still not showing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,56 @@
-var searchTerm = "";
-var searchLimit = 5;
-
-// function to search the jikan api for Naruto and log the object and title of the first hit
-var performSearch = function (term, limit) {
-  var queryURL = "https://api.jikan.moe/v3/search/anime?q=" + term + "&limit=" + limit;
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(getResults);
-};
-
-// Take in JSON response from performSearch() and parse out the searchResults
-var getResults = function (reply) {
-  var searchResults = reply.results;
-  console.log(searchResults);
-  for (var i = 0; i < searchResults.length; i++) {
-    displayResults(searchResults[i]);
-  }
-};
-
-// displayResults() to the console (needs to be to the document)
-var displayResults = function (result) {
-  // console.log("------------------------");
-  // console.log(result.title);
-  // console.log(result.image_url);
-  // console.log(result.synopsis);
-  var image = $("<img>").attr("src", result.image_url);
-  // Create the new row
-  var newRow = $("<tr>").append(
-    $("<td>").text(result.image_url),
-    $("<td>").text(result.title),
-    $("<td>").text(result.synopsis),
-  );
-
-  // Append the new row to the table
-  $("table").append(newRow);
-
-};
-
 $(document).ready(function () {
+
+  var searchTerm = "";
+  var searchLimit = 5;
+
+  // function to search the jikan api for Naruto and log the object and title of the first hit
+  var performSearch = function (term, limit) {
+    var queryURL = "https://api.jikan.moe/v3/search/anime?q=" + term + "&limit=" + limit;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(getResults);
+  };
+
+  // Take in JSON response from performSearch() and parse out the searchResults
+  var getResults = function (reply) {
+    var searchResults = reply.results;
+    console.log(searchResults);
+    for (var i = 0; i < searchResults.length; i++) {
+      displayResults(searchResults[i]);
+    }
+  };
+
+  //column titles for the table
+  var headerRow = $("<th>").append(
+    $("<td>").text("Poster"),
+    $("<td>").text("Title"),
+    $("<td>").text("Synopsis"),
+  );
+  $("table").append(headerRow);
+
+  // displayResults() to the console (needs to be to the document)
+  var displayResults = function (result) {
+    // console.log("------------------------");
+    // console.log(result.title);
+    // console.log(result.image_url);
+    // console.log(result.synopsis);
+    var imgURL = result.image_url;
+    var image = $("<img>").attr("src", imgURL);
+    console.log(image);
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(result.image),
+      $("<td>").text(result.title),
+      $("<td>").text(result.synopsis),
+    );
+
+    // Append the new row to the table
+    $("table").prepend(newRow);
+    $("table").prepend("<tr><td><br>");
+
+  };
+
   $("button").unbind().click(function (event) {
     event.preventDefault();
     newSearch = $("#search").val().trim();
