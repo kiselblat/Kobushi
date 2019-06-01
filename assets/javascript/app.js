319eb937165bf78051de9d10aaebf615bf81932e
einstein1967 <bootcamp.ejb@gmail.com>
2019-06-01 07:49:56 -0500

    table populates
    added youtube and ebay to table, .click still a problem

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,72 @@
 $(document).ready(function () {
 
-  var searchLimit = 5;
+var searchLimit = 5;
 
   // function to search the jikan api for Naruto and log the object and title of the first hit
   var performSearch = function (term, limit) {
-    var queryURL = "https://api.jikan.moe/v3/search/anime?q=" + term + "&limit=" + limit;
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(getResults);
-  };
+  var queryURL = "https://api.jikan.moe/v3/search/anime?q=" + term + "&limit=" + limit;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(getResults);
+};
 
-  var youtubeSearch = function () {
-    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=Australia&key=AIzaSyCHbM4yqSNt5FqVSFSFiMv4IaX7jxQxPJ0";
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(vidResponse){
-      var data = vidResponse.items[0];
-      var video = $("<iframe>");
-      video.attr('src', 'https://www.youtube.com/embed/' + data.id.videoId)
-      video.attr('height', "506")
-      video.attr('width', "900")
-      $('body').append(video);
-    });
-  };
-  youtubeSearch();
+var youtubeSearch = function () {
+  var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=Australia&key=AIzaSyCHbM4yqSNt5FqVSFSFiMv4IaX7jxQxPJ0";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (vidResponse) {
+    console.log(vidResponse)
+    // var data = vidResponse.items[0];
+    // var video = $("<iframe>");
+    // video.attr('src', 'https://www.youtube.com/embed/' + data.id.videoId)
+    // video.attr('height', "506")
+    // video.attr('width', "900")
+    // $('body').append(video);
+  });
+};
+youtubeSearch();
 
-  // Take in JSON response from performSearch() and parse out the searchResults
-  var getResults = function (reply) {
-    var searchResults = reply.results;
-    console.log(searchResults);
-    for (var i = 0; i < searchResults.length; i++) {
-      displayResults(searchResults[i]);
-    }
-  };
+// Take in JSON response from performSearch() and parse out the searchResults
+var getResults = function (reply) {
+  var searchResults = reply.results;
+  console.log(searchResults);
+  for (var i = 0; i < searchResults.length; i++) {
+    displayResults(searchResults[i]);
+  }
+};
 
-  //column titles for the table
-  var headerRow = $("<th>").append(
-    $("<td>").text("Poster"),
-    $("<td>").text("Title"),
-    $("<td>").text("Synopsis"),
+// displayResults() to the page
+var displayResults = function (result) {
+  var imgURL = result.image_url;
+  var image = $("<img>").attr("src", imgURL);
+  var youtube = "Watch trailer on Youtube";
+  // youtube.attr("title", result.title);
+  // youtube.click(youtubeSearch());
+  var ebay = "Search eBay for DVD";
+  ebay.click(function() {
+    // INSERT EBAY SEARCH HERE
+  });
+
+  // Create the new row
+  var newRow = $("<tr>").append(
+    $('<td id="newPage">').append(image),
+    $("<td>").text(result.title),
+    $("<td>").text(result.synopsis),
+    $("<td>").text(youtube),
+    $("<td>").text(ebay),
   );
-  $("table").append(headerRow);
 
-  // displayResults() to the page
-  var displayResults = function (result) {
-    var imgURL = result.image_url;
-    var image = $("<img>").attr("src", imgURL);
-    image.attr("title", result.title);
-
-    // Create the new row
-    var newRow = $("<tr>").append(
-      $('<td id="newPage">').append(image),
-      $("<td>").text(result.title),
-      $("<td>").text(result.synopsis),
-    );
-
-    // Append the new row to the table
-    $("table").prepend(newRow);
-    $("table").prepend("<tr><td><br>");
-
-  };
-
-  $("#newPage").click(console.log("function"));
-
+  // Append the new row to the table
+  $("table").prepend(newRow);
+  $("table").prepend("<tr><td><br>");
+};
 
   $("button").unbind().click(function (event) {
     event.preventDefault();
     newSearch = $("#search").val().trim();
-    console.log(newSearch);
+    // console.log(newSearch);
     performSearch(newSearch, searchLimit);
   });
 
