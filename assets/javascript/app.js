57ca81c0cea219d758c62590250d85b6e570ea91
einstein1967 <bootcamp.ejb@gmail.com>
2019-06-01 11:28:46 -0500

    youtube video plays
    yay!!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
 var searchLimit = 5;
+var newTitle = "";
 
   // function to search the jikan api for Naruto and log the object and title of the first hit
   var performSearch = function (term, limit) {
@@ -12,21 +13,20 @@ var searchLimit = 5;
 };
 
 var youtubeSearch = function () {
-  var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=Australia&key=AIzaSyCHbM4yqSNt5FqVSFSFiMv4IaX7jxQxPJ0";
+  var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q="+newTitle+"&key=AIzaSyCHbM4yqSNt5FqVSFSFiMv4IaX7jxQxPJ0";
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (vidResponse) {
     console.log(vidResponse)
-    // var data = vidResponse.items[0];
-    // var video = $("<iframe>");
-    // video.attr('src', 'https://www.youtube.com/embed/' + data.id.videoId)
-    // video.attr('height', "506")
-    // video.attr('width', "900")
-    // $('body').append(video);
+    var data = vidResponse.items[5];
+    var video = $("<iframe>");
+    video.attr('src', 'https://www.youtube.com/embed/' + data.id.videoId)
+    video.attr('height', "506")
+    video.attr('width', "2900")
+    $('table').prepend(video);
   });
 };
-youtubeSearch();
 
 // Take in JSON response from performSearch() and parse out the searchResults
 var getResults = function (reply) {
@@ -42,21 +42,30 @@ var displayResults = function (result) {
   var imgURL = result.image_url;
   var image = $("<img>").attr("src", imgURL);
   var youtube = "Watch trailer on Youtube";
-  // youtube.attr("title", result.title);
-  // youtube.click(youtubeSearch());
   var ebay = "Search eBay for DVD";
-  ebay.click(function() {
-    // INSERT EBAY SEARCH HERE
-  });
+
+  $(document).unbind().on('click', 'td', function(event){
+    if(event.target.title){
+      newTitle = this.title + " trailer";
+      console.log(newTitle);
+      youtubeSearch();
+    }
+  })
 
   // Create the new row
   var newRow = $("<tr>").append(
-    $('<td id="newPage">').append(image),
+    $('<td>').append(image),
     $("<td>").text(result.title),
     $("<td>").text(result.synopsis),
-    $("<td>").text(youtube),
-    $("<td>").text(ebay),
+    $("<td>").text(youtube).attr("title", result.title),
+    $("<td>").text(ebay).click(ebaySearch),
   );
+
+  
+
+  var ebaySearch=function() {
+    console.log("ebay");
+  }
 
   // Append the new row to the table
   $("table").prepend(newRow);
